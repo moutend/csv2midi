8a1854c58478cb23e04ffef7f5100787f493ccbc
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-12-16 22:18:45 +0900

    Add

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	midi "github.com/moutend/go-midi"
+	"github.com/moutend/go-midi/constant"
+	"github.com/moutend/go-midi/deltatime"
+	"github.com/moutend/go-midi/event"
+)
+
+func newDeltaTime(s string) (*deltatime.DeltaTime, error) {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return nil, err
+	}
+	return deltatime.New(i)
+}
+
+func parseLine(line string) (event.Event, error) {
+	s := strings.Split(line, ",")
+	if len(s) != 4 {
+		return nil, fmt.Errorf("line must be contain: delta time, type, note and velocity: %v", line)
+	}
+	deltaTime, err := newDeltaTime(s[0])
+	if err != nil {
+		return nil, err
+	}
+	note, err := constant.ParseNote(s[2])
+	if err != nil {
+		return nil, err
+	}
+	velocity, err := strconv.Atoi(s[3])
+	if err != nil {
+		return nil, err
+	}
+	eventName := strings.ToLower(s[1])
+	switch eventName {
+	case "on":
+		return event.NewNoteOnEvent(deltaTime, 0, note, uint8(velocity))
+	case "off":
+		return event.NewNoteOffEvent(deltaTime, 0, note, uint8(velocity))
+	default:
+		return nil, fmt.Errorf("invalid line '%v'", line)
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		return
+	}
+	file, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var events []event.Event
+	position := 0
+	lines := strings.Split(string(file), "\n")
+	lines = lines[0 : len(lines)-1]
+	for _, line := range lines {
+		e, err := parseLine(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		events = append(events, e)
+		position += int(e.DeltaTime().Quantity().Uint32())
+	}
+	var defaultTimeDivision int = 960
+	remainingDeltaTime, err := deltatime.New(position % defaultTimeDivision)
+	if err != nil {
+		log.Fatal(err)
+	}
+	endOfTrack, _ := event.NewEndOfTrackEvent(remainingDeltaTime)
+	events = append(events, endOfTrack)
+
+	track := midi.NewTrack(events...)
+	m := midi.MIDI{}
+	m.TimeDivision().SetBPM(defaultTimeDivision)
+	m.Tracks = append(m.Tracks, track)
+	fmt.Println(track)
+	if err := ioutil.WriteFile("output.mid", m.Serialize(), 0644); err != nil {
+		log.Fatal(err)
+	}
+	return
+}
